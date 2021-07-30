c64f0a81fa67ada6ad9a4e1dc81d85fc8dd87dfb
Nipun Jain <lucifernipun22@gmail.com>
2021-07-30 22:32:18 +0530

    Initial

---
+++ app/src/main/java/com/nipun/kaagaz_scanner_assignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nipun.kaagaz_scanner_assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
